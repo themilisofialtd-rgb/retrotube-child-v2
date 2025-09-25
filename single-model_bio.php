e3c9205e0cde0fabe9f342e0af9d605ef3f09bd8
themilisofialtd-rgb <themilisofialtd@gmail.com>
2025-09-25 09:32:39 +0200

    Align model templates with standard layout

--- single-model_bio.php
+++ single-model_bio.php
@@ -1,57 +1,82 @@
 <?php
+$rt_child_model_bio_embed = isset($rt_child_model_bio_embed) && $rt_child_model_bio_embed;
+
+if (!$rt_child_model_bio_embed) {
+    get_header();
+    ?>
+    <div id="primary" class="content-area container">
+      <div class="row">
+        <main id="main" class="site-main col-md-8 model-bio-page">
+          <?php get_template_part('breadcrumb'); ?>
+    <?php
+}
+
 // Get ACF fields
-$bio = get_field('bio');
-$model_link = get_field('model_link');
-$banner = get_field('banner_image');
+$bio              = get_field('bio');
+$model_link_field = get_field('model_link');
+$banner           = get_field('banner_image');
 $flipbox_shortcode = get_field('flipbox_shortcode');
-$model_title = get_the_title();
+$model_title      = get_the_title();
+
+$banner_url = '';
+$banner_alt = '';
+if (is_array($banner)) {
+    $banner_url = isset($banner['url']) && is_string($banner['url']) ? $banner['url'] : '';
+    $banner_alt = isset($banner['alt']) ? $banner['alt'] : '';
+} elseif (is_string($banner)) {
+    $banner_url = $banner;
+}
+
+$model_link_url = '';
+if (is_array($model_link_field)) {
+    $model_link_url = isset($model_link_field['url']) && is_string($model_link_field['url']) ? $model_link_field['url'] : '';
+} elseif (is_string($model_link_field)) {
+    $model_link_url = $model_link_field;
+}
 ?>
 
 <div class="model-header" style="text-align: center; margin-bottom: 30px;">
     <h1><?php the_title(); ?></h1>
 
-    <?php
-    if ($banner) {
-        $banner_url = '';
-        $banner_alt = '';
-
-        if (is_array($banner)) {
-            $banner_url = isset($banner['url']) ? $banner['url'] : '';
-            $banner_alt = isset($banner['alt']) ? $banner['alt'] : '';
-        } elseif (is_string($banner)) {
-            $banner_url = $banner;
-        }
-
-        if (!empty($banner_url)) {
-            $alt_text = !empty($banner_alt) ? $banner_alt : $model_title;
-            ?>
-            <div class="model-hero">
-                <img src="<?php echo esc_url($banner_url); ?>" alt="<?php echo esc_attr($alt_text); ?>" style="max-width:100%; height:auto; margin-top: 15px;">
-            </div>
-            <?php
-        }
-    }
-    ?>
+    <?php if (!empty($banner_url)) : ?>
+        <?php $alt_text = !empty($banner_alt) ? $banner_alt : $model_title; ?>
+        <div class="model-hero">
+            <img src="<?php echo esc_url($banner_url); ?>" alt="<?php echo esc_attr($alt_text); ?>" style="max-width:100%; height:auto; margin-top: 15px;">
+        </div>
+    <?php endif; ?>
 </div>
 
 <div class="model-content">
-    <?php if ($bio): ?>
+    <?php if ($bio) : ?>
         <div class="model-bio" style="margin-bottom: 30px;">
             <?php echo wp_kses_post($bio); ?>
         </div>
     <?php endif; ?>
 
-    <?php if ($flipbox_shortcode): ?>
+    <?php if ($flipbox_shortcode) : ?>
         <div class="model-flipbox" style="margin: 40px 0;">
             <?php echo do_shortcode($flipbox_shortcode); ?>
         </div>
     <?php endif; ?>
 
-    <?php if ($model_link): ?>
+    <?php if (!empty($model_link_url)) : ?>
         <div class="model-link" style="text-align: center; margin-top: 40px;">
-            <a href="<?php echo esc_url($model_link); ?>" class="btn" target="_blank" rel="nofollow">
+            <a href="<?php echo esc_url($model_link_url); ?>" class="btn" target="_blank" rel="nofollow">
                 Visit <?php the_title(); ?> on LiveJasmin
             </a>
         </div>
     <?php endif; ?>
 </div>
+
+<?php
+if (!$rt_child_model_bio_embed) {
+    ?>
+        </main>
+        <aside class="col-md-4">
+          <?php get_sidebar(); ?>
+        </aside>
+      </div>
+    </div>
+    <?php get_footer(); ?>
+    <?php
+}
