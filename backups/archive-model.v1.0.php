f5abcc1ba5940b8f09497d1fe07a3ff9e8864677
themilisofialtd-rgb <themilisofialtd@gmail.com>
2025-10-03 21:13:01 +0200

    Align models archive with grid template

--- backups/archive-model.v1.0.php
+++ backups/archive-model.v1.0.php
@@ -4,18 +4,12 @@
  */
 get_header();
 ?>
-<div id="content" class="site-content row">
-  <div id="primary" class="content-area with-sidebar-right models-archive">
-    <main id="main" class="site-main with-sidebar-right" role="main">
-      <?php get_template_part('breadcrumb'); ?>
-      <?php
+<div class="tmw-layout">
+  <main id="primary" class="site-main">
+    <?php
       echo do_shortcode('[actors_flipboxes per_page="12" cols="3" show_pagination="true" page_var="pg"]');
-      ?>
-    </main>
-  </div>
-  <aside id="sidebar" class="widget-area with-sidebar-right" role="complementary">
-    <?php get_sidebar(); ?>
-  </aside>
+    ?>
+  </main>
+  <?php get_sidebar(); ?>
 </div>
-<?php
-get_footer();
+<?php get_footer(); ?>
